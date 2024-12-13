b716235f7b04a84edf71ed4e40f67529ba838727
Zaida <zaida_cortes@soy.sena.edu.co>
2024-12-12 21:48:13 -0500

    se agrega proyecto

---
+++ base de datos Online_school.sql
@@ -0,0 +1,6 @@
+SELECT * FROM online_school.representante_padredefamilia;
+insert into representante_padredefamilia( idRepresentante,cedula,Nombre,Email_personal,Dirección,Celular) values ( "02","45678234","Ernesto","ernestomendoza89@gmail.com","Calle65c#56-11sur","3145973497");
+insert into representante_padredefamilia( idRepresentante,cedula,Nombre,Email_personal,Dirección,Celular) values ( "03","1013372056","Luisa","luisa45@gmail.com","Carrera76c#23-81","3248973496");
+insert into representante_padredefamilia( idRepresentante,cedula,Nombre,Email_personal,Dirección,Celular) values ( "04","72567428","erman","ermansalinas@gmail.com","Diagonal5c#78-12sur","3215973483");
+insert into representante_padredefamilia( idRepresentante,cedula,Nombre,Email_personal,Dirección,Celular) values ( "05","1045678654","Laura","LauraPeveda@hotmail.com","Calle89c#16-71","3224797349");
+select * from representante_padredefamilia;
